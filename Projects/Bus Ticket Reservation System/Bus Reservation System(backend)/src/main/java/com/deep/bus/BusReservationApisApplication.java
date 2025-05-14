62d2c6d221b6bcc2f0c8b716fd1dd32db374cbd4
DOHITH VIGNESHWAR SP <139889599+Dohithvigneshwar@users.noreply.github.com>
2025-05-14 12:31:20 +0530

    Add files via upload

---
+++ Projects/Bus Ticket Reservation System/Bus Reservation System(backend)/src/main/java/com/deep/bus/BusReservationApisApplication.java
@@ -0,0 +1,12 @@
+package com.deep.bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusReservationApisApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BusReservationApisApplication.class, args);
+	}
+
+}
---
+++ Projects/Bus Ticket Reservation System/Bus Reservation System(backend)/src/main/java/com/deep/bus/service/AdminService.java
@@ -0,0 +1,8 @@
+package com.deep.bus.service;
+
+import com.deep.bus.exception.*;
+import com.deep.bus.entities.*;
+
+public interface AdminService {
+    public Admin registerAdmin(Admin admin) throws AdminException;
+}
